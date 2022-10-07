8dd4518f0b79562ee0094e0d51b45b4fc4a9e810
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-10-07 14:20:27 +0300

    Create birinciodev.py
    verilen ilk odev

---
+++ birinciodev.py
@@ -0,0 +1,13 @@
+#Zehra Şahaplıoğlu 02190201010
+sayi = input('Bir Sayı Giriniz : ')
+ 
+#Sayının pozitif mi negatif mi sıfır mı?
+ 
+if(int(sayi)>0):
+     print(sayi +" Sayısı Pozitif Sayıdır.")
+ 
+if(int(sayi)<0):
+     print(sayi +" Sayısı Negatif Sayıdır.")
+ 
+if(int(sayi)==0):
+     print(sayi +" Sayısı Sıfırdır.")
